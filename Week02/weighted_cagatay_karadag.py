d69b62deee767aff3a27d3444718364563723e9d
cagataykrdg <167318124+cagataykrdg@users.noreply.github.com>
2026-04-13 14:24:55 +0300

    Refactor weighted_srs function for clarity

--- Week02/weighted_cagatay_karadag.py
+++ Week02/weighted_cagatay_karadag.py
@@ -1,8 +1,7 @@
 import random
 
 def weighted_srs(data, n, weights, with_replacement=False):
-    if with_replacement:
-      return random.choices(data, weights=weights, k=n) 
-    if not weights: 
-      return random.sample(data, n) 
-    return random.sample(data, n, counts=weights)
+    if with_replacement or weights:
+        return random.choices(data, weights=weights, k=n)
+    else:
+        return random.sample(data,n)
